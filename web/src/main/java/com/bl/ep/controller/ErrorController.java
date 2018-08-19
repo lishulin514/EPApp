a6ffe6a31b03593ab90e34be88d21244e465e97c
lishulin514 <769458908@qq.com>
2018-08-20 00:05:04 +0800

    更改包名

--- web/src/main/java/com/bl/ep/controller/ErrorController.java
+++ web/src/main/java/com/bl/ep/controller/ErrorController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ErrorController {
 
     @RequestMapping("/404")
-    public String center(){
+    public String error404(){
 
         return "/error/404";
+    }
+
+    @RequestMapping("/500")
+    public String error500(){
+
+        return "/error/500";
     }
 }
